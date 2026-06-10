ab877ce0f4cc56a210cc17eb6eca49eb3dfba1f7
P0onch0 <144670555+P0onch0@users.noreply.github.com>
2026-06-10 11:16:05 +0200

    [RED] Test CaféNonServi_QuandCléRechargeable - la machine ne doit pas reagir aux cles non prepayees

--- MachineACafé.Test/NfcPaymentTest.cs
+++ MachineACafé.Test/NfcPaymentTest.cs
@@ -24,6 +24,24 @@ public class NfcPaymentTest
     }
 
     [Fact]
+    public void CaféNonServi_QuandCléRechargeable()
+    {
+        // ETANT DONNE une machine à café
+        var nfc = new NfcTransceiverFake(chargeResult: true);
+        var brewer = new BrewerSpy();
+        _ = new SoftwareMachineBuilder()
+            .AyantUnNfcTransceiver(nfc)
+            .AyantUnBrewer(brewer)
+            .Build();
+
+        // QUAND une clé rechargeable (non pré-payée) est présentée
+        nfc.SimulerApparitionCle(NfcState.RefillableDevicePresent);
+
+        // ALORS aucun café n'est préparé et TryChargeAmount n'est pas appelé
+        Assert.Equal(0, brewer.MakeACoffeeInvocations);
+    }
+
+    [Fact]
     public void CaféNonServi_QuandSoldeInsuffisant()
     {
         // ETANT DONNE une machine à café avec une clé NFC sans solde suffisant
