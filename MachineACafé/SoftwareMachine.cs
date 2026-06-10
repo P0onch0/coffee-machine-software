d6c938333ccc716798c571c3b13acaeb6f484469
P0onch0 <144670555+P0onch0@users.noreply.github.com>
2026-06-10 11:15:13 +0200

    [GREEN] SoftwareMachine souscrit a NfcStateChanged et appelle TryChargeAmount(40) + MakeACoffee

--- MachineACafé/SoftwareMachine.cs
+++ MachineACafé/SoftwareMachine.cs
@@ -6,12 +6,21 @@ public class SoftwareMachine
 {
     private readonly IBrewer _brewer;
     private readonly IChangeMachine _changeMachine;
+    private readonly INfcTransceiver _nfcTransceiver;
 
-    public SoftwareMachine(IBrewer brewer, IChangeMachine changeMachine)
+    public SoftwareMachine(IBrewer brewer, IChangeMachine changeMachine, INfcTransceiver nfcTransceiver)
     {
         _brewer = brewer;
         _changeMachine = changeMachine;
+        _nfcTransceiver = nfcTransceiver;
         _changeMachine.RegisterMoneyInsertedCallback(coin => Insérer(new Coin((ushort) coin)));
+        _nfcTransceiver.NfcStateChanged += PaiementNfc;
+    }
+
+    private void PaiementNfc(NfcState état)
+    {
+        _nfcTransceiver.TryChargeAmount(40);
+        _brewer.MakeACoffee();
     }
 
     private void Insérer(Coin somme)
